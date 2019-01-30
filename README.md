0d8592c5dd24308a96a475e657dc808ad91491ad
coconaut <alexanderbarry.dev@gmail.com>
2019-01-30 17:41:50 -0500

    added files

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# go-bigger
+# gobigger
 Repo with large LFS files, this time with another go.mod to prune
---
+++ client/client.go
@@ -0,0 +1,7 @@
+package client
+
+import "fmt"
+
+func Stuff() {
+	fmt.Println("This is the gobigger client")
+}
